5ce97ccf2a6ae5bee5bbd3c3a56437c5be3a817b
Pedro Henrique <80739159+Phnasc@users.noreply.github.com>
2023-08-17 02:07:21 -0300

    Add files via upload

---
+++ script_typed.js
@@ -0,0 +1,15 @@
+
+var script = document.createElement('script');
+script.src = 'https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js';
+document.head.appendChild(script);
+
+
+script.onload = function() {
+
+  var typed = new Typed('.auto-input', {
+    strings: ['Pedro Henrique', 'a Developer', 'a Sea lover', 'almost Engineer'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+  });
+};
